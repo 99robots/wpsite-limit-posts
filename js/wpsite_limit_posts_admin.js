8e34e860dae056e74bc71683df599322293c0fbc
Mohammed Saduzzaman Sadi <64192420+zamans78@users.noreply.github.com>
2021-04-21 17:20:04 +0600

    Changed  jQuery.fn.change() event shorthand which was deprecated.

--- js/wpsite_limit_posts_admin.js
+++ js/wpsite_limit_posts_admin.js
@@ -1,21 +1,21 @@
-jQuery(document).ready(function($) {
-
-	$("input:radio[name=wpsite_limit_posts_settings_all_users]").change(function(){
-		if ($(this).val() == 'capability') {
-			$(".wpsite_limit_posts_users").hide();
-			$(".wpsite_limit_posts_roles").show();
-		} else {
-			$(".wpsite_limit_posts_users").show();
-			$(".wpsite_limit_posts_roles").hide();
+jQuery(document).ready(function ($) {
+  
+	$("input:radio[name=wpsite_limit_posts_settings_all_users]").on("change", function () {
+			if ($(this).val() == "capability") {
+				$(".wpsite_limit_posts_users").hide();
+				$(".wpsite_limit_posts_roles").show();
+			} else {
+				$(".wpsite_limit_posts_users").show();
+				$(".wpsite_limit_posts_roles").hide();
+			}
 		}
-	});
+	);
 
-	if ($("input:radio[name=wpsite_limit_posts_settings_all_users]:checked").val() == 'capability') {
+	if ($("input:radio[name=wpsite_limit_posts_settings_all_users]:checked").val() == "capability") {
 		$(".wpsite_limit_posts_users").hide();
 		$(".wpsite_limit_posts_roles").show();
 	} else {
 		$(".wpsite_limit_posts_users").show();
 		$(".wpsite_limit_posts_roles").hide();
 	}
-
 });
